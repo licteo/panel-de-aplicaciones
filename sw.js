a0c01863e719747ef2bce59b265c23794b3ad7cd
licteo <134066234+licteo@users.noreply.github.com>
2024-12-07 17:58:57 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'panel-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/app1.html',
+  '/app2.html'
+];
+
+// Instalar el Service Worker y almacenar los archivos en caché
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Interceptar solicitudes y servir desde la caché
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Actualizar la caché si hay cambios
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
